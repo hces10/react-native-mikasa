34386f118d9edd590ea1826b3eaf9835fac890ba
Henrique Cesar <hces10@gmail.com>
2019-11-19 22:48:09 -0300

    arduino sync

--- components/MidSensor.js
+++ components/MidSensor.js
@@ -1,9 +1,11 @@
+/* eslint-disable global-require */
+/* eslint-disable no-confusing-arrow */
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 
 export default class MidSensor extends Component {
-  state = { response: {} }
+  state = { response: {}, state: true, isUp: [] }
 
   componentWillMount() {
     this.getState();
@@ -16,8 +18,9 @@ export default class MidSensor extends Component {
     axios.get('http://177.193.46.190:8082')
       .then((response) => {
         // handle success
-        console.log(response);
-        this.setState({ response });
+        const { isUp } = response.data.split(',');
+        console.log(isUp);
+        this.setState({ response, isUp });
       })
       .catch((error) => {
         // handle error
@@ -26,27 +29,51 @@ export default class MidSensor extends Component {
       })
       .finally(() => {
         // always executed
-      });
+	    });
   }
+
+  handleToogle = () => {
+    const axios = require('axios');
+    if (this.state.state)
+      return this.setState({ state: !this.state.state },
+        () => axios.post('http://177.193.46.190:8082/off1', {})
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      );
+    return this.setState({ state: !this.state.state }, () => axios.post('http://177.193.46.190:8082/on1', {})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    );
+  }
+  
 
 	render() {
 		return (
 			<View style={styles.fundo}>	
-
-			<View style={styles.fundoComponente}>
-				<View style={styles.saida}>
-						<View style={styles.viewNomeSensor}>
-							<Text style={styles.textoMid}>{this.state.response.data}</Text>
-						</View>
-						
-						<View style={styles.viewEstado}>
-							<Image source={require('../img/IconeRed.jpg')} style={{ width: 200, height: 100 }} />
-						</View>
-					
-				</View>
-				
-
-			</View>	
+        <View style={styles.fundoComponente}>
+          <View style={styles.saida}>
+            <View style={styles.viewNomeSensor}>
+              <Text style={styles.textoMid}>{this.state.response.data}</Text>
+            </View>
+            <TouchableOpacity
+              style={styles.viewEstado}
+              onPress={() => this.handleToogle()}
+            >
+              <Image source={this.state.state
+              ? require('../img/IconeGreen.jpg')
+              : require('../img/IconeRed.jpg')}
+              style={{ width: 200, height: 100 }} />
+            </TouchableOpacity>
+          </View>
+        </View>	
 			</View>
 		);
 	}
@@ -84,11 +111,10 @@ const styles = StyleSheet.create({
 		flex: 1
 	},
 	textoMid: {
-		fontSize: 5,
+		fontSize: 15,
 		color: 'blue',
 		fontWeight: 'bold',
 		textAlign: 'center',
 		fontFamily: 'sans-serif'
     }
 });
-
